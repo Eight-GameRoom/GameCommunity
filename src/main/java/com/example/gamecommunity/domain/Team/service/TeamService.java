5f75e312984630ebeb7d3111f0a9e990f51e216a
minseong1 <alstjd2627@naver.com>
2024-01-09 13:52:05 +0900

    refactor : User -> UserDetailsImp

--- src/main/java/com/example/gamecommunity/domain/Team/service/TeamService.java
+++ src/main/java/com/example/gamecommunity/domain/Team/service/TeamService.java
@@ -3,6 +3,7 @@ package com.example.gamecommunity.domain.Team.service;
 import com.example.gamecommunity.domain.Team.dto.TeamRequestDto;
 import com.example.gamecommunity.domain.Team.dto.TeamResponseDto;
 import com.example.gamecommunity.domain.user.entity.User;
+import com.example.gamecommunity.global.security.userdetails.UserDetailsImpl;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -10,27 +11,27 @@ import org.springframework.stereotype.Service;
 public class TeamService {
 
 
-  public List<TeamResponseDto> getTeam(User user) {
+  public List<TeamResponseDto> getTeam(UserDetailsImpl userDetails) {
 
     return null;
   }
-  public List<TeamResponseDto> getTeam(User user, Long userId) {
+  public List<TeamResponseDto> getTeam(UserDetailsImpl userDetails, Long userId) {
 
     return null;
   }
 
-  public void createTeam(User user, TeamRequestDto teamRequestDto) {
+  public void createTeam(UserDetailsImpl userDetails, TeamRequestDto teamRequestDto) {
   }
 
-  public void deleteTeam(User user, Long teamId) {
+  public void deleteTeam(UserDetailsImpl userDetails, Long teamId) {
   }
 
-  public void updateTeam(User user, Long teamId, TeamRequestDto teamRequestDto) {
+  public void updateTeam(UserDetailsImpl userDetails, Long teamId, TeamRequestDto teamRequestDto) {
   }
 
-  public void addUserToTeam(User user, Long teamId, Long userId) {
+  public void addUserToTeam(UserDetailsImpl userDetails, Long teamId, Long userId) {
   }
 
-  public void deleteUserFromTeam(User user, Long teamId, Long userId) {
+  public void deleteUserFromTeam(UserDetailsImpl userDetails, Long teamId, Long userId) {
   }
 }
