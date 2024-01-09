3dc5367de03a463a94f093da5e06c3d69b0e9180
minseong1 <alstjd2627@naver.com>
2024-01-09 16:07:24 +0900

    feat : GameEnum(임시)

---
+++ src/main/java/com/example/gamecommunity/domain/Team/GameEnum.java
@@ -0,0 +1,21 @@
+package com.example.gamecommunity.domain.Team;
+
+import lombok.Getter;
+
+@Getter
+public enum GameEnum {
+  MINECRAFT("Minecraft"),
+  FORTNITE("Fortnite"),
+  PUBG("PlayerUnknown's Battlegrounds"),
+  LEAGUE_OF_LEGENDS("League of Legends"),
+  OVERWATCH("Overwatch");
+
+  // 게임 이름을 반환하는 메서드
+  private final String gameName;  // 게임 이름을 나타내는 변수
+
+  // 생성자
+  GameEnum(String gameName) {
+    this.gameName = gameName;
+  }
+
+}
