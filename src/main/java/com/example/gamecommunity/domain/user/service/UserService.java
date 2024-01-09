aad81e42d02dbae3c966861eb08676cd54a13376
Kolin136 <jsowb136@gmail.com>
2024-01-09 20:45:56 +0900

    feat:"이메일 인증 번호 발송 기능 추가"

--- src/main/java/com/example/gamecommunity/domain/user/service/UserService.java
+++ src/main/java/com/example/gamecommunity/domain/user/service/UserService.java
@@ -1,9 +1,7 @@
 package com.example.gamecommunity.domain.user.service;
 
-import static com.example.gamecommunity.global.exception.common.ErrorCode.ALREADY_EXIST_USER_EMAIL_EXCEPTION;
-import static com.example.gamecommunity.global.exception.common.ErrorCode.FAILED_AUTHENTICATION_EXCEPTION;
-import static com.example.gamecommunity.global.exception.common.ErrorCode.NOT_EQUALS_CONFIRM_PASSWORD;
-import static com.example.gamecommunity.global.exception.common.ErrorCode.NOT_FOUND_USER_EXCEPTION;
+import static com.example.gamecommunity.global.exception.common.ErrorCode.*;
+
 
 import com.example.gamecommunity.domain.user.dto.LoginRequestDto;
 import com.example.gamecommunity.domain.user.dto.PasswordChangeRequestDto;
@@ -14,8 +12,14 @@ import com.example.gamecommunity.domain.user.repository.UserRepository;
 import com.example.gamecommunity.global.exception.common.BusinessException;
 import com.example.gamecommunity.global.security.userdetails.UserDetailsImpl;
 import com.example.gamecommunity.global.util.JwtUtil;
+import com.example.gamecommunity.global.util.RandomNumber;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -30,8 +34,12 @@ public class UserService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JavaMailSender mailSender;
 
   private final JwtUtil jwtUtil;
+
+  @Value("${spring.mail.username}")
+  private String senderEmail;
 
   public void signup(SignupRequestDto requestDto) {
     emailCheck(requestDto.email());
@@ -42,6 +50,27 @@ public class UserService {
 
     userRepository.save(user);
   }
+
+  public void emailSend(String toEmail) {
+    //JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
+    MimeMessage message = mailSender.createMimeMessage();
+    String authNumber = RandomNumber.createNumber();
+    String content = "인증 번호:"+authNumber;
+
+    try {
+      // true를 전달하여 multipart 형식의 메시지를 지원, "utf-8"을 전달하여 문자 인코딩을 설정
+      MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+
+      helper.setFrom(this.senderEmail);//이메일의 발신자 주소 설정
+      helper.setTo(toEmail);//이메일의 수신자 주소 설정
+      helper.setSubject("게임 커뮤니티 회원 가입 인증 이메일");//이메일의 제목을 설정
+      helper.setText(content,true);//이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
+      mailSender.send(message);
+    } catch (MessagingException e) {//이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
+      throw  new BusinessException(HttpStatus.BAD_REQUEST,FAILED_EMAIL_SEND_EXCEPTION);
+    }
+  }
+
 
   public TokenDto login(LoginRequestDto requestDto) {
 
@@ -72,12 +101,12 @@ public class UserService {
 
     //로그인중 유저 패스워드랑 request에 담긴 변경전 패스워드랑 같은지 체크
     if (!(passwordEncoder.matches(requestDto.nowPassword(), user.getPassword()))) {
-      throw new BusinessException(HttpStatus.BAD_REQUEST,FAILED_AUTHENTICATION_EXCEPTION );
+      throw new BusinessException(HttpStatus.BAD_REQUEST,FAILED_AUTHENTICATION_EXCEPTION);
     }
 
     //변경할 비번,비번 확인 같은지 체크
     else if (!(requestDto.newPassword().equals(requestDto.checkPassword()))) {
-      throw new BusinessException(HttpStatus.BAD_REQUEST, NOT_EQUALS_CONFIRM_PASSWORD);
+      throw new BusinessException(HttpStatus.BAD_REQUEST, NOT_EQUALS_CONFIRM_PASSWORD_EXCEPTION);
     }
 
     user.updatePassword(password);
@@ -92,7 +121,7 @@ public class UserService {
 
   public void confirmPassword(SignupRequestDto requestDto) {
     if (!(requestDto.password().equals(requestDto.checkPassword()))) {
-      throw new BusinessException(HttpStatus.BAD_REQUEST, NOT_EQUALS_CONFIRM_PASSWORD);
+      throw new BusinessException(HttpStatus.BAD_REQUEST, NOT_EQUALS_CONFIRM_PASSWORD_EXCEPTION);
     }
   }
 
@@ -111,7 +140,5 @@ public class UserService {
     SecurityContext context = SecurityContextHolder.getContext();
     context.setAuthentication(authentication);
   }
-
-
 
 }
