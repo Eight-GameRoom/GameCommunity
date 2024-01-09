fd9c11bc65e353b7295f617ab45168f4a1321f78
oneseel <seno1006@gmail.com>
2024-01-09 12:17:56 +0900

    feat : Post, User 연관관계 설정

--- src/main/java/com/example/gamecommunity/domain/post/entity/Post.java
+++ src/main/java/com/example/gamecommunity/domain/post/entity/Post.java
@@ -10,9 +10,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -41,7 +44,7 @@ public class Post extends TimeStamped {
   @Column
   private String postImageUrl;
 
-  @Column
+  @Column(nullable = false)
   private String postAuthor;
 
   @Column
@@ -65,6 +68,10 @@ public class Post extends TimeStamped {
   @Column(nullable = false)
   private Integer postUnlike;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
   public Post(PostRequestDto requestDto, GameType gameType, GameName gameName,
       BoardName boardName, String postImageUrl, User loginUser) {
     this.postId = getPostId();
@@ -78,6 +85,7 @@ public class Post extends TimeStamped {
     this.report = 0;
     this.postLike = 0;
     this.postUnlike = 0;
+    this.user = loginUser;
   }
 
   public void update(PostRequestDto requestDto, String postImageUrl) {
