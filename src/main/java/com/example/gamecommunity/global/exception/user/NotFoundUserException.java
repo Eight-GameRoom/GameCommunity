19afb66dbe27881392b1ce5857b0254a148a9ddd
Dahee Son <sxxdxh2@gmail.com>
2024-01-10 15:44:57 +0900

    test : cicd테스트

--- src/main/java/com/example/gamecommunity/global/exception/user/NotFoundUserException.java
+++ src/main/java/com/example/gamecommunity/global/exception/user/NotFoundUserException.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 public class NotFoundUserException extends BusinessException {
   public NotFoundUserException() {
     super(HttpStatus.valueOf(400),ErrorCode.NOT_FOUND_USER_EXCEPTION);
+
   }
 
 }
