0cf86fb13235ceb2cf483eb0f9b1387f6e5fcbfc
oneseel <seno10065@gmail.com>
2024-01-11 01:34:21 +0900

    test : 게시글 생성(성공) API 테스트

---
+++ src/test/java/com/example/gamecommunity/domain/common/CommonTest.java
@@ -0,0 +1,52 @@
+package com.example.gamecommunity.domain.common;
+
+import com.example.gamecommunity.domain.enums.boardName.BoardName;
+import com.example.gamecommunity.domain.enums.gameName.GameName;
+import com.example.gamecommunity.domain.enums.gameType.GameType;
+import com.example.gamecommunity.domain.post.entity.Post;
+import com.example.gamecommunity.domain.user.entity.User;
+
+public interface CommonTest {
+
+  String ANOTHER_PREFIX = "another-";
+
+  // post
+  Long TEST_POST_ID = 1L;
+  Long TEST_ANOTHER_POST_ID = 2L;
+  String TEST_POST_TITLE = "title";
+  String TEST_POST_CONTENT = "content";
+  String TEST_POST_IMAGE_URL = "url";
+  String TEST_POST_AUTHOR = "username";
+  GameType GAME_TYPE = GameType.PC_GAME;
+  GameName GAME_NAME = GameName.LEAGUE_OF_LEGEND;
+  BoardName BOARD_NAME = BoardName.FREE_BOARD;
+  Integer REPORT = 0;
+  Integer POST_LIKE = 0;
+  Integer POST_Unlike = 0;
+
+  Post TEST_POST = Post.builder()
+      .postTitle(TEST_POST_TITLE)
+      .postContent(TEST_POST_CONTENT)
+      .postImageUrl(TEST_POST_IMAGE_URL)
+      .postAuthor(TEST_POST_AUTHOR)
+      .gameType(GAME_TYPE)
+      .gameName(GAME_NAME)
+      .boardName(BOARD_NAME)
+      .report(REPORT)
+      .postLike(POST_LIKE)
+      .postUnlike(POST_Unlike)
+      .build();
+
+  Post TEST_ANOTHER_POST = Post.builder()
+      .postTitle(ANOTHER_PREFIX + TEST_POST_TITLE)
+      .postContent(ANOTHER_PREFIX + TEST_POST_CONTENT)
+      .postImageUrl(ANOTHER_PREFIX + TEST_POST_IMAGE_URL)
+      .postAuthor(ANOTHER_PREFIX + TEST_POST_AUTHOR)
+      .gameType(GAME_TYPE)
+      .gameName(GAME_NAME)
+      .boardName(BOARD_NAME)
+      .report(REPORT)
+      .postLike(POST_LIKE)
+      .postUnlike(POST_Unlike)
+      .build();
+}
---
+++ src/test/java/com/example/gamecommunity/domain/post/service/PostServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.gamecommunity.domain.post.service;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+import com.example.gamecommunity.domain.common.CommonTest;
+import com.example.gamecommunity.domain.post.dto.PostRequestDto;
+import com.example.gamecommunity.domain.post.entity.Post;
+import com.example.gamecommunity.domain.post.repository.PostRepository;
+import com.example.gamecommunity.domain.user.entity.User;
+import java.io.IOException;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+@DisplayName("게시글 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+  @InjectMocks
+  private PostService postService;
+  @Mock
+  private PostRepository postRepository;
+  @Mock
+  private PostImageUploadService postImageUploadService;
+
+  @Nested
+  @DisplayName("게시글 작성 테스트")
+  class CreatePostTest implements CommonTest {
+
+    @Test
+    @DisplayName("게시글 작성 - 성공")
+    void createPostTestSuccess() throws IOException {
+
+      // given
+      PostRequestDto requestDto = new PostRequestDto(TEST_POST_TITLE, TEST_POST_CONTENT);
+      MultipartFile file = mock(MultipartFile.class);
+      User loginUser = mock(User.class);
+
+      given(postImageUploadService.uploadFile(file)).willReturn(TEST_POST_IMAGE_URL);
+
+      // when
+      postService.createPost(requestDto, GAME_TYPE, GAME_NAME, BOARD_NAME, file, loginUser);
+
+      // Then
+      verify(postImageUploadService, times(1)).uploadFile(file);
+      verify(postRepository, times(1)).save(any(Post.class));
+    }
+  }
+}
