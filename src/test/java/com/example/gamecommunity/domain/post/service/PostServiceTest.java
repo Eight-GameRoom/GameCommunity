d7a36eb218a38a235ef7c3c550fba1937ab38c99
oneseel <seno10065@gmail.com>
2024-01-11 12:48:52 +0900

    fix : 환경변수 수정

--- src/test/java/com/example/gamecommunity/domain/post/service/PostServiceTest.java
+++ src/test/java/com/example/gamecommunity/domain/post/service/PostServiceTest.java
@@ -14,9 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
 @DisplayName("게시글 서비스 테스트")
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
