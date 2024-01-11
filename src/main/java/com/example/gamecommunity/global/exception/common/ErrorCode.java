6d32e145ca886aa73ee046da86ea53fa1c9ddf71
oneseel <seno10065@gmail.com>
2024-01-11 18:02:50 +0900

    fix : 비회원일 때, 로그인이 필요한 기능을 사용할 때 예외처리

--- src/main/java/com/example/gamecommunity/global/exception/common/ErrorCode.java
+++ src/main/java/com/example/gamecommunity/global/exception/common/ErrorCode.java
@@ -21,6 +21,7 @@ public enum ErrorCode {
   FAILED_EMAIL_SEND_EXCEPTION("이메일 서버 문제 or 잘못된 이메일 주소 입니다"),
   FAILED_EMAIL_AUTHENTICATION_EXCEPTION("이메일 인증번호 일치하지 않습니다."),
   EMAIL_VERIFICATION_NEEDED("이메일 인증이 필요 합니다"),
+  AUTHENTICATION_EXCEPTION("로그인하고 이용해주세요."),
   AUTHENTICATION_MISMATCH_EXCEPTION("수정 및 삭제 권한이 없습니다.");
 
   private final String message;
