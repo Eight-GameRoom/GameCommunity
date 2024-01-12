5f038318112df824237b129d43fdd1fda932be0f
minseong1 <alstjd2627@naver.com>
2024-01-12 17:55:03 +0900

    refactor : comment table명 수정

--- src/main/java/com/example/gamecommunity/domain/comment/entity/Comment.java
+++ src/main/java/com/example/gamecommunity/domain/comment/entity/Comment.java
@@ -12,6 +12,7 @@ import java.sql.Timestamp;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "comments")
 public class Comment extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
